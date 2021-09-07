1f90e1ff43d4ec5bf6c445a498976c7d48326d2a
Cyaegha96 <zazae51@gmail.com>
2021-09-07 13:21:19 +0900

    1.6 Nweet update delete

---
+++ src/components/Ntweets.js
@@ -0,0 +1,69 @@
+import { dbService } from "fbase";
+import { useState } from "react";
+
+const Nweet = ({ nweetObj, isOwner }) => {
+
+    const [editing, setEditing] = useState(false); //수정 버튼 클릭시 입력란과 버튼이 나타나는 기준점 ; 토글상태
+    const [newNweet, setNewNweet] = useState(nweetObj.text); //
+
+
+    const onDeleteClick = async () => {
+        const ok = window.confirm("삭제하시겠습니까?");
+        if (ok) {
+            console.log(ok);
+            const data = await dbService.doc(`nweets/${nweetObj.id}`).delete();
+
+        }
+    }
+
+    const toggleEditing = () => setEditing((prev) => !prev);
+
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewNweet(value);
+    }
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.doc(`nweets/${nweetObj.id}`).update({ text: newNweet });
+        setEditing(false);
+    }
+
+    return (
+        <div>
+            {editing ? (
+                <>
+                    <form onSubmit={onSubmit}>
+                        <input
+                            value={newNweet}
+                            onChange={onChange}
+                            required />
+                        <input
+                            type="submit"
+                            value="Update Nweet"
+
+                        />
+                    </form>
+                    <button onClick={toggleEditing}>Cancel</button>
+                </>
+
+            ) : (
+                <>
+                    <h4>{nweetObj.text}</h4>
+                    {
+                        isOwner && (
+                            <>
+                                <button onClick={onDeleteClick}>Delete Nweet</button>
+                                <button onClick={toggleEditing}>Update Nweet</button>
+                            </>
+                        )
+                    }
+                </>
+            )}
+        </div>
+    );
+};
+
+export default Nweet;
